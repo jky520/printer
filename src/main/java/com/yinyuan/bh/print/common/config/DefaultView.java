60cd3f78020c98a1236d63914ad3f03bfc377f32
jky520 <jky1988@qq.com>
2017-12-15 17:55:35 +0800

    添加说明

--- src/main/java/com/yinyuan/bh/print/common/config/DefaultView.java
+++ src/main/java/com/yinyuan/bh/print/common/config/DefaultView.java
@@ -9,12 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * 默认首页的设置
  * Created by DT人 on 2017/12/15 11:24.
  */
-@Configuration
+//@Configuration
 public class DefaultView extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         System.out.println("进来了");
-        registry.addViewController("/" ).setViewName("forward:/login");
+        registry.addViewController("/" ).setViewName("forward:/login.html");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE );
         System.out.println("进来了============》");
         super.addViewControllers(registry);
